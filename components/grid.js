d5cd5f10959ec856882c39b5481c7768ab26d526
lch <lch24tsinghua@qq.com>
2025-02-17 17:23:42 +0800

    feat: shit

--- components/grid.js
+++ components/grid.js
@@ -1,3 +1,5 @@
+import Canvas from "../interfaces/canvas.js";
+import { Canvases } from "../interfaces/enum.js";
 import Handler from "../interfaces/handler.js";
 
 const Grid = {
@@ -23,7 +25,37 @@ const Grid = {
     this.draw();
   },
 
-  draw() {},
+  draw() {
+    // grid: [{value, ...}]
+    const ctx = Canvas.getCtx(Canvases.Grid);
+    const { cw, ch } = Canvas.getSize();
+    const ratio = Canvas.getRatio();
+    ctx.clearRect(0, 0, cw, ch);
+    if (!this.grid) return;
+    const col = this.grid.length;
+    const row = this.grid[0].length;
+    const gw = (32 * col + 12 * row) * ratio;
+    const gh = 24 * row * ratio;
+    const padding = 48 * ratio;
+    const scaleX = (cw - 2 * padding) / gw;
+    const scaleY = (ch - 2 * padding) / gh;
+    if (scaleX < scaleY) {
+      const startY = (ch - gh * scaleY) / 2;
+      ctx.transform(scaleX, 0, 0, scaleX, padding, startY);
+    } else {
+      const startX = (cw - gw * scaleX) / 2;
+      ctx.transform(scaleY, 0, 0, scaleY, startX, padding);
+    }
+    {
+      ctx.fillStyle = "black";
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 2 * ratio;
+      ctx.lineJoin = "round";
+      ctx.beginPath();
+      const width = 32 * ratio;
+      this.grid.forEach((col) => col.forEach((land) => {}));
+    }
+  },
 
   setTexture(img) {
     this.texture = img;
